ea55ca01f46979c56030d9495ac4208e03559bee
Amarnath466 <sachuamar4645@gmail.com>
2024-07-30 19:06:17 +0530

    I made it
    It is my first project

--- README.py
+++ README.py
@@ -1,1 +1,11 @@
-print("Hello")
+print("Quiz")
+
+print("Do you want to play quiz. If yes enter yes else no")
+
+a = input("Enter : ")
+
+if a == 'yes':
+    print("Your first question ")
+    print("Q1:  Who is the current persident of India?")
+b = input("Enter 1 for Sree Narendra")
+
